d5c8a1e190d68fafc8b02b9e10d0b41ef9ecf7b5
zJoyceLee <zJoyceLee@yeah.net>
2016-03-25 11:03:16 +0800

    NumericalCalculation:Course Homework.

---
+++ NumericalCalculation/Chap01/Chap01_05.py
@@ -0,0 +1,6 @@
+num=int(input("Please input a integer(>99): "))
+if num > 99:
+    output_num = num/100
+    print("int hund is %s"%output_num)
+else:
+    print("The num is less than 100.")
---
+++ NumericalCalculation/Chap05/Chap05_04.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    try:
+    except e:
+        print(e)
