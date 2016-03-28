078dca39dd6fc24c925f73e2ac1d951ff2c7e6b5
zJoyceLee <zJoyceLee@yeah.net>
2016-03-28 09:36:19 +0800

    Use isinstance(x, type) to judge x is int. Add isallnum().

--- NumericalCalculation/Chap01/Chap01_05.py
+++ NumericalCalculation/Chap01/Chap01_05.py
@@ -1,6 +1,27 @@
-num=int(input("Please input a integer(>99): "))
-if num > 99:
-    output_num = num/100
-    print("int hund is %s"%output_num)
-else:
-    print("The num is less than 100.")
+# python3 ./xx.py
+
+def isallnum(ss):
+    for i in ss:
+        if i < '0' or i > '9':
+            return False
+    return True
+
+
+if __name__=='__main__':
+    try:
+        ss = input("Please input a integer(>99): ")
+
+        if isallnum(ss):
+            num=eval(ss)
+
+            if isinstance(num, int):
+                if num > 99:
+                    mytuple = divmod(num, 100)
+                    print("int hund is %d"%mytuple[0])
+                else:
+                    print("The num is less than 100.")
+        else:
+            print('Input Error:: not number.')
+
+    except (TypeError, NameError) as e:
+        print(e)
