c3b1c639c05795f6605c32dae036a8c07f549fab
zJoyceLee <zJoyceLee@yeah.net>
2016-03-30 17:48:40 +0800

    Django:Tutorial: writing your first Django app, part7. Add Choice.

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,                      {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
 
 admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
