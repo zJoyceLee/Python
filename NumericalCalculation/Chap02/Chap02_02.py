9d573525ee5f7e5e4e15a2cd4347682834acc259
zJoyceLee <zJoyceLee@yeah.net>
2016-05-21 11:13:10 +0800

    Migrate code to Homework Repo.

--- NumericalCalculation/Chap02/Chap02_02.py
+++
@@ -1,9 +0,0 @@
-import random
-lst=[]
-for i in range(0, 1000):
-    lst.append(random.randint(1, 100))
-#    print(lst)
-for i in range(1, 100):
-    if i in lst:
-        counter=lst.count(i)
-        print("%d frequency of occurrence is : %d"%(i,counter))
